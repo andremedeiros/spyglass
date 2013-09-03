6708f07f08340efc6af18ec59f481630c35cd004
Andre Medeiros <me@andremedeiros.info>
2013-09-03 13:02:53 +0200

    Fixed the color constants for YCrCb.

--- ext/spyglass/color_space.cc
+++ ext/spyglass/color_space.cc
@@ -7,7 +7,7 @@ namespace Spyglass {
     void define_ruby_module() {
       ColorSpaceModule = rb_define_module_under(Spyglass::get_ruby_module(), "ColorSpace");
 
-      // BGR      
+      // BGR
       rb_define_const(ColorSpaceModule, "BGR_TO_GRAY",  INT2NUM(CV_BGR2GRAY));
       rb_define_const(ColorSpaceModule, "BGR_TO_HLS",   INT2NUM(CV_BGR2HLS));
       rb_define_const(ColorSpaceModule, "BGR_TO_HSV",   INT2NUM(CV_BGR2HSV));
@@ -30,10 +30,10 @@ namespace Spyglass {
       rb_define_const(ColorSpaceModule, "RGB_TO_HLS",   INT2NUM(CV_RGB2HLS));
       rb_define_const(ColorSpaceModule, "RGB_TO_HSV",   INT2NUM(CV_RGB2HSV));
       rb_define_const(ColorSpaceModule, "RGB_TO_YCRCB", INT2NUM(CV_RGB2YCrCb));
-      
+
       // YCrCb
-      rb_define_const(ColorSpaceModule, "YCRCB2BGR", INT2NUM(CV_YCrCb2BGR));
-      rb_define_const(ColorSpaceModule, "YCRCB2RGB", INT2NUM(CV_YCrCb2RGB));
+      rb_define_const(ColorSpaceModule, "YCRCB_TO_BGR", INT2NUM(CV_YCrCb2BGR));
+      rb_define_const(ColorSpaceModule, "YCRCB_TO_RGB", INT2NUM(CV_YCrCb2RGB));
     }
   }
 }
